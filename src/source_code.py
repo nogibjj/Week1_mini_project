1e14d890573204de6d7c71d031594e06605c8e30
johncoogan53 <141885470+johncoogan53@users.noreply.github.com>
2023-09-01 16:18:21 +0000

    "adding source code template for stability"

---
+++ src/source_code.py
@@ -0,0 +1,17 @@
+# test source code to make the python template worflow stable
+
+
+def add(x, y):
+    var1 = x
+    var2 = y
+    return x + y
+
+
+def main():
+    a = 1
+    b = 2
+    return add(a, b)
+
+
+if __name__ == "__main__":
+    main()
